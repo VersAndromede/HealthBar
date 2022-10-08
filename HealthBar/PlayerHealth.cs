ab488f9460b079583b83d62d3a9c6d267da07eb4
VersAndromede <102908677+VersAndromede@users.noreply.github.com>
2022-10-08 17:09:21 +0300

    Add files via upload

---
+++ HealthBar/PlayerHealth.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [field: SerializeField] public float MaxHealth { get; private set; }
+
+    public UnityAction Changed;
+
+    public float Health { get; private set; }
+
+    private void Start()
+    {
+        Health = MaxHealth;
+        Changed?.Invoke();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+            return;
+
+        if (Health - damage >= 0)
+            Health -= damage;
+        else
+            Health = 0;
+
+        Changed?.Invoke();
+    }
+
+    public void AddHealth(float health)
+    {
+        if (health > 0 && Health + health <= MaxHealth)
+        {
+            Health += health;
+            Changed?.Invoke();
+        }
+    }
+}
